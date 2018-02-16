5cce63a0c9c3a1aafc7946ed309af9f0956a4bc1
Hugo LIEGEARD <hugo@hl-media.co>
2018-02-16 13:09:46 +0100

    04-B - TEMPLATING DANS SF4
    Génération des vues
     - Affichage de la Page d'Accueil
     - Affichage des Articles d'une Catégorie
     - Affichage d'un Article du site.

--- LES-FONDAMENTAUX/02-CREATION-PROJET-SF-4/monPremierProjetSF/assets/app.js
+++ LES-FONDAMENTAUX/02-CREATION-PROJET-SF-4/monPremierProjetSF/assets/app.js
@@ -1,8 +1,8 @@
 // -- Chargement du CSS
-import './css/main.scss';
+require('./css/main.scss');
 
 // -- Chargement du JS
-import './js/jquery.min';
-import './js/bootstrap.js';
-import './js/owl.carousel.min.js';
-import './js/main.js';
+require('./js/jquery.min');
+require('./js/bootstrap.js');
+require('./js/owl.carousel.min.js');
+require('./js/main.js');
--- LES-FONDAMENTAUX/02-CREATION-PROJET-SF-4/monPremierProjetSF/src/Controller/TechNews/IndexController.php
+++ LES-FONDAMENTAUX/02-CREATION-PROJET-SF-4/monPremierProjetSF/src/Controller/TechNews/IndexController.php
@@ -14,27 +14,30 @@ class IndexController extends Controller
      * Configuration de notre route grâce dans routes.yaml
      */
     public function index() {
-        # return new Response("<html><body><h1>PAGE D'ACCUEIL</h1></body></html>");
         return $this->render('index/index.html.twig');
     }
 
     /**
      * Page permettant d'afficher les articles d'une catégorie
-     * @Route("/categorie/{libellecategorie}", name="index_categorie", methods={"GET"})
+     * @Route("/categorie/{libellecategorie}",
+     *     name="index_categorie",
+     *     methods={"GET"},
+     *     requirements={"libellecategorie"="\w+"})
      * @param $libellecategorie
      * @return Response
      */
-    public function categorie($libellecategorie = 'computing') {
-        return new Response("<html><body><h1>PAGE CATEGORIE : $libellecategorie</h1></body></html>");
+    public function categorie($libellecategorie = 'tout') {
+        return $this->render('index/categorie.html.twig');
     }
 
     /**
      * Page permettant d'afficher un article
      * @see https://symfony.com/doc/current/routing.html#adding-wildcard-requirements
-     * @Route("/{libellecategorie}/{slugarticle}_{idarticle}.html", name="index_article", requirements={"idarticle"="\d+"} )
+     * @Route("/{libellecategorie}/{slugarticle}_{idarticle}.html", name="index_article",
+     *     requirements={"idarticle"="\d+"} )
      */
     public function article($libellecategorie, $slugarticle, $idarticle) {
         # index.php/business/une-formation-symfony-a-paris_98426852.html
-        return new Response("<html><body><h1>PAGE ARTICLE : $libellecategorie | $slugarticle | $idarticle</h1></body></html>");
+        return $this->render('index/article.html.twig');
     }
 }
